038bdfb87d95773acf30d6c15ec8cdca73b0c4d1
Colo1396 <nazarenogalvan@outlook.com>
2021-08-21 12:23:13 -0300

    A ver si ahora funciona heroku

--- NetCoreAPIMySQL/NetCoreAPIMySQL/Program.cs
+++ NetCoreAPIMySQL/NetCoreAPIMySQL/Program.cs
@@ -6,14 +6,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace NetCoreAPIMySQL
 {
     public class Program
     {
-        public static void Main(string[] args)
+        /*public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+        }*/
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+              .UseKestrel()
+              .UseContentRoot(Directory.GetCurrentDirectory())
+              .UseConfiguration(config)
+              .UseIISIntegration()
+              .UseStartup<Startup>()
+              .Build();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
